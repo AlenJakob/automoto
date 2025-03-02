37e1aa5dec2cb64cf516b4a20bb9e4966479e6f2
alen.jakob <grafuxa@gmail.com>
2025-03-02 21:44:18 +0100

    feat: add type

---
+++ src/hooks/useGetOffers.ts
@@ -0,0 +1,38 @@
+
+import supabase from "@/lib/supabase/supabase";
+import { PostgrestError, PostgrestSingleResponse } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+type Offer = {
+	title: string;
+	description: string;
+	image: string;
+	user_id: string;
+	created_at: string;
+	price: number;
+
+}
+
+const useGetOffers = () => {
+	const [offers, setOffers] = useState<Offer[]>([]);
+	const [error, setError] = useState<PostgrestError | null | string>(null);
+
+	useEffect(() => {
+		const getAllOffers = async () => {
+
+			const { data, error }: PostgrestSingleResponse<Offer[]> = await supabase
+				.from('offers')
+				.select('*')
+
+			setOffers(data ?? []);
+			setError(error?.message ?? null);
+		}
+
+
+		getAllOffers()
+	}, []);
+
+	return { offers, error }
+}
+
+export default useGetOffers
