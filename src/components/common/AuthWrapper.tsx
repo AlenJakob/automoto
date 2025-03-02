1fdea04489c3905df3b15b39c25ed63b1224a700
alen.jakob <grafuxa@gmail.com>
2025-03-02 21:44:40 +0100

    feat: update wrapper for auth form

--- src/components/common/AuthWrapper.tsx
+++ src/components/common/AuthWrapper.tsx
@@ -1,17 +1,28 @@
-import { Box } from "@chakra-ui/react/box";
+import { Box, Text } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
 
-const AuthWrapper = ({ children }: { children: ReactNode }) => {
+const AuthWrapper = ({
+	children,
+	title,
+}: {
+	children: ReactNode;
+	title?: string;
+}) => {
 	return (
 		<Box
 			w="100%"
-			maxW="400px"
+			maxW="500px"
 			mx="auto"
 			mt={10}
 			p={5}
 			borderWidth={1}
 			borderRadius="lg"
 		>
+			{title && (
+				<Text fontSize="xl" fontWeight="bold" textAlign="center" mb={4}>
+					{title}
+				</Text>
+			)}
 			{children}
 		</Box>
 	);
